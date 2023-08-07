4d323bd7f2bfe54d23d2ae6d5b9f4ae62fa1d844
ConradKash <kakurucon1234@gmail.com>
2023-08-07 22:21:52 +0300

    Get

--- app/main.py
+++ app/main.py
@@ -15,6 +15,10 @@ class PredictionOut(BaseModel):
 def root():
     return {"Model Version": model_Version}
 
+@app.get("/{entText}")
+def transApi(entText):
+    return {"The language is in": predictm(entText)}
+
 @app.post("/predict", response_model=PredictionOut)
 def predict(payload: TextIn):
     language = predictm(payload.text)
