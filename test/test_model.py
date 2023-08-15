b5aead677046f7fee198cbc9db3af7c4fa787ef7
ConradKash <kakurucon1234@gmail.com>
2023-08-15 15:58:37 +0300

    Testing updated

--- test/test_model.py
+++ test/test_model.py
@@ -1,7 +1,7 @@
 from app.model import predicted_language
 
 def test_acholi():
-    assert predicted_language("I Amuru aa ki California i Amerika ento.") == 'Acholi'
+    assert predicted_language("Cang oyot.") == 'Acholi'
     
     
 def test_english():
