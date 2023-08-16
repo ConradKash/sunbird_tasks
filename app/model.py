da21078fcb6289a02a4a56d4dc3181e8c8ca9eb4
ConradKash <kakurucon1234@gmail.com>
2023-08-16 14:15:29 +0300

    compress

--- app/model.py
+++ app/model.py
@@ -4,18 +4,11 @@ import nltk
 import joblib
 nltk.download('punkt')
 from nltk.tokenize import word_tokenize
-
-
-BASE_DIR = Path(__file__).resolve(strict=True).parent
-
-   
+BASE_DIR = Path(__file__).resolve(strict=True).parent   
 with open(f"{BASE_DIR}/model.pkl", "rb") as f:
-    model = pickle.load(f)
-   
-     
+    model = pickle.load(f)      
 with open(f"{BASE_DIR}/transform.pkl", "rb") as f:
-    cv = pickle.load(f)
-    
+    cv = pickle.load(f)    
 classes = [
     'Acholi',
     'English',
@@ -24,14 +17,12 @@ classes = [
     'Runyankole',
     'Ateso'
 ]
-
 def text_processing(translate_text):
     translate_text = translate_text.lower()
     translate_text = translate_text.split()
     translate_text = word_tokenize(str(translate_text))
     translate_text = ' '.join(translate_text)
-    return translate_text     
-
+    return translate_text   
 def predicted_language(ptext):
     process_text = text_processing(ptext)
     x = cv.transform([process_text]).toarray()
