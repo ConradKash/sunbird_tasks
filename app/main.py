a426920c8973459437b414c12f1ef10d4d6e44a7
ConradKash <kakurucon1234@gmail.com>
2023-08-19 23:58:52 +0300

    put endput

--- app/main.py
+++ app/main.py
@@ -23,50 +23,7 @@ def predict(payload: TextIn):
     predict_language = predicted_language(payload.text)
     return PredictionOut(language = predict_language)
 
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-    tags: list[str] = []
+@app.put("/predict")
+def deliverable(textinput: TextIn, language_id: PredictionOut):
+    return {"item_name": textinput.text, "item_id": language_id.language}
 
-
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item) -> Any:
-    return item
-
-
-@app.get("/items/", response_model=list[Item])
-async def read_items() -> Any:
-    return [
-        {"name": "Portal Gun", "price": 42.0},
-        {"name": "Plumbus", "price": 32.0},
-    ]
-from datetime import datetime, time, timedelta
-from typing import Annotated
-from uuid import UUID
-
-from fastapi import Body, FastAPI
-
-app = FastAPI()
-
-
-@app.put("/items/{item_id}")
-async def read_items(
-    item_id: UUID,
-    start_datetime: Annotated[datetime | None, Body()] = None,
-    end_datetime: Annotated[datetime | None, Body()] = None,
-    repeat_at: Annotated[time | None, Body()] = None,
-    process_after: Annotated[timedelta | None, Body()] = None,
-):
-    start_process = start_datetime + process_after
-    duration = end_datetime - start_process
-    return {
-        "item_id": item_id,
-        "start_datetime": start_datetime,
-        "end_datetime": end_datetime,
-        "repeat_at": repeat_at,
-        "process_after": process_after,
-        "start_process": start_process,
-        "duration": duration,
-    }
