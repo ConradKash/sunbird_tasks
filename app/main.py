08441fbd54adf596ef8520e0526a8869b4b8bbe9
ConradKash <kakurucon1234@gmail.com>
2023-08-19 22:30:17 +0300

    I am going to use lists

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from app.model import _version__ as model_Version, predicted_language
-
+from app.model import predicted_language
+from typing import Any
 
 app = FastAPI()
 
-x = 1
+
 class TextIn(BaseModel):
     text: str
     
@@ -14,17 +14,31 @@ class PredictionOut(BaseModel):
     language: str
     
     
-@app.get("/")
+@app.get("/")   
 def root():
-    return {"Model Version": model_Version}
-
-
-@app.get("/{text}")
-def trans_api(text):
-    return {"The language is in": predicted_language(text)}
-
+    return {"Model Version LanguageID app"}
 
 @app.post("/predict", response_model = PredictionOut)
 def predict(payload: TextIn):
     predict_language = predicted_language(payload.text)
     return PredictionOut(language = predict_language)
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list[str] = []
+
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item) -> Any:
+    return item
+
+
+@app.get("/items/", response_model=list[Item])
+async def read_items() -> Any:
+    return [
+        {"name": "Portal Gun", "price": 42.0},
+        {"name": "Plumbus", "price": 32.0},
+    ]
