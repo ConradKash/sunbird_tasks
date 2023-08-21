224d78cc4a9cec13b385d38107c9cd45f62b7b5c
ConradKash <kakurucon1234@gmail.com>
2023-08-22 01:18:37 +0300

    Tapp here

--- app/main.py
+++ app/main.py
@@ -1,30 +1,31 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from app.model import predicted_language
-from typing import Any
+from time import perf_counter
 
 app = FastAPI()
 
-
-class TextIn(BaseModel):
+    
+class Prediction(BaseModel):
     text: str
-    
-    
-class PredictionOut(BaseModel):
     language: str
-    
-class UserPost(BaseModel):
-    name: str
-    body: str
+    proocess_time: float
     
     
 @app.get("/")   
 def root():
     return {"Model Version LanguageID app"}
 
-@app.post("/predict", response_model = PredictionOut)
-def predict(payload: TextIn):
+@app.post("/predict", response_model = Prediction)
+def predict(payload: Prediction):
+    start_time = perf_counter()
+    
     predict_language = predicted_language(payload.text)
-    return PredictionOut(language = predict_language)
+    
+    end_time = perf_counter()
+    
+    procrss_time = end_time - start_time
+    return Prediction(text = payload.text, language = predict_language, proocess_time = procrss_time)
 
 
